86f0b49be23c41cb7c565f2ee84641e050f477e9
Evan <evan@chequer.io>
2020-06-22 23:12:21 +0900

    Add antlr build script

---
+++ Build/Build.Antlr.ps1
@@ -0,0 +1,31 @@
+
+Set-Location $(Get-Item "$PSScriptRoot").Parent.FullName
+
+$AntlrJar = Resolve-Path ".\Build\antlr-4.8-complete.jar"
+
+Get-ChildItem -Path "Qsi.*" -Directory | ForEach-Object {
+    $Namespace = $PSItem.Name
+    $GrammarDirectory = [System.IO.Path]::Combine($PSItem.FullName, "Antlr")
+    $OutputDirectory = [System.IO.Path]::Combine($GrammarDirectory, "generated")
+
+    if (!(Test-Path $GrammarDirectory)) {
+        Write-Host "Skip $($PSItem.Name)" -ForegroundColor DarkGray
+        continue
+    }
+
+    Write-Host "[Antlr4] Build $($PSItem.Name).." -ForegroundColor Green
+
+    # Clean output
+    if (Test-Path $OutputDirectory) {
+        Remove-Item -Path $OutputDirectory -Recurse -Force -Confirm:$false -ErrorAction Ignore
+    }
+
+    java `
+        -jar "$AntlrJar" `
+        -Dlanguage=CSharp `
+        -package "$Namespace" `
+        -Xexact-output-dir `
+        -o "$OutputDirectory" `
+        -visitor `
+        "$GrammarDirectory\*.g4"
+}
