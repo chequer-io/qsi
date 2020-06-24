1664f0db569be56d5101f1f69165006b79f6d0e3
Evan <evan@chequer.io>
2020-06-24 14:58:31 +0900

    Add MySqlParser

---
+++ Qsi.MySql/MySqlParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antlr4.Runtime;
+using Qsi.Data;
+using Qsi.MySql.Internal;
+using Qsi.Parsing.Antlr;
+using Qsi.Tree;
+
+namespace Qsi.MySql
+{
+    public sealed class MySqlParser : AntlrParserBase
+    {
+        protected override Parser CreateParser(QsiScript script)
+        {
+            var stream = new AntlrUpperInputStream(script.Script);
+            var lexer = new MySqlLexer(stream);
+            var tokens = new CommonTokenStream(lexer);
+            return new Internal.MySqlParser(tokens);
+        }
+
+        protected override IQsiTreeNode ParseTree(QsiScript script, Parser parser)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<QsiScript> ParseScripts(string script)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
