450110fb1b07dfa7843ec37e65bd33be04f7dd83
Evan <evan@chequer.io>
2020-06-30 17:02:22 +0900

    Add qsi playground

---
+++ Qsi.Playground/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Qsi.Data;
+using Qsi.MySql;
+using Qsi.Parsing;
+using Qsi.Playground.Utilities;
+using Qsi.Tree;
+
+namespace Qsi.Playground
+{
+    public class MainWindow : Window
+    {
+        private readonly ComboBox _cbLanguages;
+        private readonly TextBox _tbInput;
+        private readonly TextBlock _tbError;
+        private readonly TreeView _tvQsi;
+
+        private readonly Dictionary<string, Lazy<IQsiParser>> _parsers;
+
+        private IQsiParser _qsiParser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _parsers = new Dictionary<string, Lazy<IQsiParser>>
+            {
+                ["MySQL"] = new Lazy<IQsiParser>(() => new MySqlParser())
+            };
+
+            _cbLanguages = this.Find<ComboBox>("cbLanguages");
+            _tbInput = this.Find<TextBox>("tbInput");
+            _tbError = this.Find<TextBlock>("tbError");
+            _tvQsi = this.Find<TreeView>("tvQsi");
+
+            _cbLanguages.SelectionChanged += CbLanguagesOnSelectionChanged;
+            _cbLanguages.Items = _parsers.Keys;
+            _cbLanguages.SelectedIndex = 0;
+
+            _tbInput.KeyDown += TbInputOnKeyDown;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void CbLanguagesOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_parsers.TryGetValue((string)_cbLanguages.SelectedItem, out Lazy<IQsiParser> parser))
+            {
+                _qsiParser = parser.Value;
+                Update();
+            }
+            else
+            {
+                _qsiParser = null;
+            }
+        }
+
+        private void TbInputOnKeyDown(object sender, KeyEventArgs e)
+        {
+            Flow.Debounce(sender, Update, 500);
+        }
+
+        private void Update()
+        {
+            ClearError();
+            ClearVisualTree();
+
+            try
+            {
+                if (_qsiParser == null || string.IsNullOrWhiteSpace(_tbInput.Text))
+                    return;
+
+                var input = _tbInput.Text;
+
+                // TODO: change to IQsiScriptParser
+                var sentence = input.Split(';', 2)[0].Trim();
+                var script = new QsiScript(sentence, QsiScriptType.Select);
+
+                var tree = _qsiParser.Parse(script);
+                BuildVisualTree(tree);
+            }
+            catch (Exception e)
+            {
+                _tbError.Text = e.ToString();
+            }
+        }
+
+        private void ClearError()
+        {
+            _tbError.Text = null;
+        }
+
+        private void ClearVisualTree()
+        {
+            _tvQsi.Items = null;
+        }
+
+        #region Qsi TreeView
+        private void BuildVisualTree(IQsiTreeNode node)
+        {
+            _tvQsi.Items = new[] { BuildVisualTreeImpl(node) };
+        }
+
+        private TreeViewItem BuildVisualTreeImpl(IQsiTreeNode node)
+        {
+            var nodeItem = new TreeViewItem
+            {
+                Header = $"{node.GetType().Name}",
+                Background = Brushes.Aquamarine
+            };
+
+            var nodeItemChild = new List<TreeViewItem>();
+            nodeItem.Items = nodeItemChild;
+
+            foreach (var property in node.GetType().GetTypeInfo().DeclaredProperties)
+            {
+                var item = new TreeViewItem
+                {
+                    Header = property.Name
+                };
+
+                nodeItemChild.Add(item);
+
+                var value = property.GetValue(node);
+
+                switch (value)
+                {
+                    case IQsiTreeNode childNode:
+                        item.Items = new[] { BuildVisualTreeImpl(childNode) };
+                        break;
+
+                    case IEnumerable<IQsiTreeNode> childNodes:
+                        item.Items = childNodes.Select(BuildVisualTreeImpl).ToArray();
+                        break;
+
+                    default:
+                        item.Header = $"{property.Name}: {value} (terminal node)";
+                        item.Background = Brushes.IndianRed;
+                        break;
+                }
+            }
+
+            return nodeItem;
+        }
+        #endregion
+    }
+}
---
+++ Qsi.Playground/Utilities/Flow.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Qsi.Playground.Collections;
+
+namespace Qsi.Playground.Utilities
+{
+    public static class Flow
+    {
+        public delegate bool Predicate();
+
+        public delegate Task<bool> AsyncPredicate();
+
+        private static readonly ConcurrentHashSet<object> ThrottleKeys;
+        private static readonly ConcurrentDictionary<object, CancellationTokenSource> DebounceTokens;
+
+        static Flow()
+        {
+            ThrottleKeys = new ConcurrentHashSet<object>();
+            DebounceTokens = new ConcurrentDictionary<object, CancellationTokenSource>();
+        }
+
+        private static bool ThottleInternal(object key, int milliseconds = 1000)
+        {
+            if (ThrottleKeys.Contains(key))
+                return false;
+
+            if (milliseconds > 0)
+            {
+                ThrottleKeys.Add(key);
+
+                Task.Delay(milliseconds).ContinueWith(_ =>
+                {
+                    ThrottleKeys.Remove(key);
+                });
+            }
+
+            return true;
+        }
+
+        public static void Throttle(object key, Action action, int milliseconds = 1000)
+        {
+            if (ThottleInternal(key, milliseconds))
+            {
+                action();
+            }
+        }
+
+        public static async Task ThrottleAsync(object key, Func<Task> asyncAction, int milliseconds = 1000)
+        {
+            if (ThottleInternal(key, milliseconds))
+            {
+                await asyncAction();
+            }
+        }
+
+        public static void Debounce(object key, Action action, int milliseconds = 1000)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            if (DebounceTokens.TryRemove(key, out var tokenSource))
+            {
+                tokenSource.Cancel();
+            }
+
+            if (milliseconds == 0)
+            {
+                action.Invoke();
+                return;
+            }
+
+            tokenSource = new CancellationTokenSource();
+            DebounceTokens.TryAdd(key, tokenSource);
+
+            var taskScheduler = SynchronizationContext.Current != null
+                ? TaskScheduler.FromCurrentSynchronizationContext()
+                : TaskScheduler.Current;
+
+            Task.Delay(milliseconds, tokenSource.Token).ContinueWith(_ =>
+                {
+                    action.Invoke();
+                    DebounceTokens.TryRemove(key, out tokenSource);
+                },
+                tokenSource.Token,
+                TaskContinuationOptions.ExecuteSynchronously,
+                taskScheduler);
+        }
+
+        public static bool Retry(Predicate predicate, int tryCount, int tryDelay = 1000, bool throwException = false)
+        {
+            do
+            {
+                try
+                {
+                    if (predicate())
+                        return true;
+                }
+                catch (Exception)
+                {
+                    if (throwException)
+                        throw;
+                }
+
+                Thread.Sleep(tryDelay);
+            } while (tryCount-- > 0);
+
+            return false;
+        }
+
+        public static async Task<bool> RetryAsync(AsyncPredicate predicate, int tryCount, int tryDelay = 1000, bool throwException = false)
+        {
+            do
+            {
+                try
+                {
+                    if (await predicate())
+                        return true;
+                }
+                catch (Exception)
+                {
+                    if (throwException)
+                        throw;
+                }
+
+                await Task.Delay(tryDelay);
+            } while (tryCount-- > 0);
+
+            return false;
+        }
+
+        public static void TryInvoke(Action action, Action<Exception> exceptionCallback = null)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                exceptionCallback?.Invoke(e);
+            }
+        }
+
+        public static T TryInvoke<T>(Func<T> func, Func<Exception, T> exceptionCallback = null)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception e)
+            {
+                if (exceptionCallback != null)
+                    return exceptionCallback(e);
+            }
+
+            return default;
+        }
+
+        public static async Task TryInvokeAsync(Task task, Action<Exception> exceptionCallback = null)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                exceptionCallback?.Invoke(e);
+            }
+        }
+
+        public static async Task<T> TryInvokeAsync<T>(Task<T> task, Func<Exception, T> exceptionCallback = null)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                if (exceptionCallback != null)
+                    return exceptionCallback(e);
+            }
+
+            return default;
+        }
+    }
+}
