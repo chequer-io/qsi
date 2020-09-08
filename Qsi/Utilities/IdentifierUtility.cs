7253871f1c0317b379519067454268999da76d2d
Evan <evan@chequer.io>
2020-09-08 19:21:20 +0900

    Impl identifier parser

--- Qsi/Utilities/IdentifierUtility.cs
+++ Qsi/Utilities/IdentifierUtility.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Qsi.Utilities
 {
     public static class IdentifierUtility
     {
-        private static readonly char[] _openParen = { '\'', '"', '`' };
-        private static readonly char[] _closeParen = { '\'', '"', '`' };
+        private static readonly char[] _openParen = { '\'', '"', '`', '[' };
+        private static readonly char[] _closeParen = { '\'', '"', '`', ']' };
 
         private static readonly char[] _escapedChars = { '\'', '"', '`', '\\', 'n', 'r', 't', 'b', '0' };
         private static readonly char[] _escapeChars = { '\'', '"', '`', '\\', '\n', '\r', '\t', '\b', '\0' };
+
+        public static bool IsEscaped(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < 2)
+                return false;
+
+            int index = Array.IndexOf(_openParen, value[0]);
+
+            if (index == -1)
+                return false;
+
+            return value[^1] == _closeParen[index];
+        }
 
         public static string Unescape(string value)
         {
@@ -62,6 +77,76 @@ namespace Qsi.Utilities
             }
 
             return new string(buffer, 0, index);
+        }
+
+        public static string[] Parse(string value)
+        {
+            var result = new List<string>(4);
+            ReadOnlySpan<char> span = value.AsSpan();
+            var buffer = new StringBuilder();
+            char? closeParen = null;
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                var c = span[i];
+                bool end = i == buffer.Length - 1;
+
+                switch (c)
+                {
+                    case '.' when !closeParen.HasValue:
+                        result.Add(buffer.ToString());
+                        buffer.Clear();
+                        break;
+
+                    case '\\':
+                        int escapeIndex = end ? -1 : Array.IndexOf(_escapedChars, span[i + 1]);
+
+                        // ignore
+                        if (escapeIndex == -1)
+                            continue;
+
+                        buffer.Append(_escapeChars[escapeIndex]);
+                        i++;
+                        break;
+
+                    case var _ when closeParen.HasValue && c == closeParen.Value:
+                        if (!end && span[i + 1] == c)
+                        {
+                            buffer.Append(c);
+                            i++;
+                        }
+                        else
+                        {
+                            closeParen = null;
+                        }
+
+                        break;
+
+                    case '\'':
+                    case '"':
+                    case '`':
+                    case '[':
+                        if (closeParen.HasValue)
+                        {
+                            goto default;
+                        }
+                        else
+                        {
+                            closeParen = _closeParen[Array.IndexOf(_openParen, c)];
+                        }
+
+                        break;
+
+                    default:
+                        buffer.Append(c);
+                        break;
+                }
+            }
+
+            if (span[^1] != '.')
+                result.Add(buffer.ToString());
+
+            return result.ToArray();
         }
     }
 }
