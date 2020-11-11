88d1cc2831b2ee8301cf2e33f9033fb39d3096ce
Evan <evan@chequer.io>
2020-11-11 18:00:24 +0900

    Add dynamic column node

---
+++ Qsi/Tree/Immutable/Columns/ImmutableDynamicColumnNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Qsi.Data;
+using Qsi.Tree.Data;
+
+namespace Qsi.Tree.Immutable
+{
+    public readonly struct ImmutableDynamicColumnNode : IQsiDynamicColumnNode, IQsiTerminalNode
+    {
+        public IQsiTreeNode Parent { get; }
+
+        public QsiQualifiedIdentifier Name { get; }
+
+        public IUserDataHolder UserData { get; }
+
+        public IEnumerable<IQsiTreeNode> Children => Enumerable.Empty<IQsiTreeNode>();
+
+        public ImmutableDynamicColumnNode(IQsiTreeNode parent, QsiQualifiedIdentifier name, IUserDataHolder userData)
+        {
+            Parent = parent;
+            Name = name;
+            UserData = userData;
+        }
+    }
+}
