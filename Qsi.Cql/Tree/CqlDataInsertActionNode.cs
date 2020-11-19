f074f5e68b3834880a2e0973d2bfc7427bb56b0b
Evan <evan@chequer.io>
2020-11-19 22:06:31 +0900

    Add extended cql nodes

---
+++ Qsi.Cql/Tree/CqlDataInsertActionNode.cs
@@ -0,0 +1,9 @@
+﻿using Qsi.Tree;
+
+namespace Qsi.Cql.Tree
+{
+    public class CqlDataInsertActionNode : QsiDataInsertActionNode
+    {
+        public string Json { get; set; }
+    }
+}
---
+++ Qsi.Cql/Tree/CqlDerivedTableNode.cs
@@ -0,0 +1,13 @@
+﻿using Qsi.Tree;
+
+namespace Qsi.Cql.Tree
+{
+    public sealed class CqlDerivedTableNode : QsiDerivedTableNode
+    {
+        public bool IsJson { get; set; }
+
+        public bool AllowFiltering { get; set; }
+
+        public int? PerPartitionLimit { get; set; }
+    }
+}
