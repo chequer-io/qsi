647bd9164be1581968e8c9ede6ae2bfec86861ea
Evan <evan@chequer.io>
2020-11-30 23:21:33 +0900

    Impl json table result

--- Qsi.Cql/Analyzers/CqlTableAnalzyer.cs
+++ Qsi.Cql/Analyzers/CqlTableAnalzyer.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Qsi.Analyzers;
+using Qsi.Analyzers.Context;
 using Qsi.Analyzers.Table;
 using Qsi.Analyzers.Table.Context;
 using Qsi.Cql.Tree;
@@ -12,6 +15,33 @@ namespace Qsi.Cql.Analyzers
     {
         public CqlTableAnalzyer(QsiEngine engine) : base(engine)
         {
+        }
+
+        protected override async ValueTask<IQsiAnalysisResult> OnExecute(IAnalyzerContext context)
+        {
+            if (context.Tree is CqlDerivedTableNode cqlTableNode && cqlTableNode.IsJson)
+            {
+                using var scope = new TableCompileContext(context);
+                var table = await BuildTableStructure(scope, cqlTableNode);
+
+                var jsonTable = new QsiTableStructure
+                {
+                    Identifier = table.Identifier,
+                    Type = table.Type,
+                    IsSystem = table.IsSystem
+                };
+
+                jsonTable.References.AddRange(table.References);
+
+                var jsonColumn = jsonTable.NewColumn();
+
+                jsonColumn.Name = new QsiIdentifier("[json]", false);
+                jsonColumn.References.AddRange(table.Columns);
+
+                return new CqlJsonTableAnalysisResult(jsonTable);
+            }
+
+            return await base.OnExecute(context);
         }
 
         protected override IEnumerable<QsiTableColumn> ResolveColumnsInExpression(TableCompileContext context, IQsiExpressionNode expression)
