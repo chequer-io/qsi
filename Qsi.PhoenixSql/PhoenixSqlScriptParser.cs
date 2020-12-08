39881bce34449271dedc8d0f5640838c6347f90f
Evan <evan@chequer.io>
2020-12-08 16:34:40 +0900

    Add Upsert type

--- Qsi.PhoenixSql/PhoenixSqlScriptParser.cs
+++ Qsi.PhoenixSql/PhoenixSqlScriptParser.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Qsi.Data;
-using Qsi.Parsing.Common;
+﻿using Qsi.Parsing.Common;
 
 namespace Qsi.PhoenixSql
 {
     public sealed class PhoenixSqlScriptParser : CommonScriptParser
     {
-        private const string upsert = "UPSERT";
-
-        protected override QsiScriptType GetSuitableType(CommonScriptCursor cursor, IReadOnlyList<Token> tokens, Token[] leadingTokens)
-        {
-            if (upsert.Equals(cursor.Value[leadingTokens[0].Span], StringComparison.OrdinalIgnoreCase))
-                return QsiScriptType.Insert;
-
-            return base.GetSuitableType(cursor, tokens, leadingTokens);
-        }
     }
 }
