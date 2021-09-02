b4362cdca242ab077413c2b53dfad588d1db5498
nnnnoel <noel@chequer.io>
2021-09-02 16:26:44 +0900

    WIP insertInto

---
+++ Qsi.Trino/Tree/Visitors/ActionVisitor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Qsi.Data;
+using Qsi.Shared.Extensions;
+using Qsi.Tree;
+using Qsi.Trino.Internal;
+
+namespace Qsi.Trino.Tree.Visitors
+{
+    using static SqlBaseParser;
+
+    internal static class ActionVisitor
+    {
+        public static QsiDataInsertActionNode VisitInsertStatement(InsertIntoContext context)
+        {
+            var name = context.qualifiedName();
+            var columnAliases = context.columnAliases();
+            var query = context.query();
+
+            var node = TrinoTree.CreateWithSpan<QsiDataInsertActionNode>(context);
+
+            var targetNode = TrinoTree.CreateWithSpan<QsiTableReferenceNode>(context);
+            targetNode.Identifier = name.qqi;
+            node.Target.Value = targetNode;
+
+            if (columnAliases is not null)
+                node.Columns.AddRange(columnAliases.identifier().Select(identifierContext => new QsiQualifiedIdentifier(identifierContext.qi)));
+
+            node.
+            
+            return node;
+        }
+    }
+}
---
+++ Qsi.Trino/TrinoParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using Qsi.Data;
+using Qsi.Parsing;
+using Qsi.Tree;
+using Qsi.Trino.Internal;
+using Qsi.Utilities;
+
+namespace Qsi.Trino
+{
+    using static SqlBaseParser;
+
+    public class TrinoParser : IQsiTreeParser
+    {
+        public IQsiTreeNode Parse(QsiScript script, CancellationToken cancellationToken = default)
+        {
+            var parser = TrinoUtility.CreateParser(script.Script);
+
+            var statement = parser.singleStatement().statement();
+
+            return ParseTrinoStatement(statement);
+        }
+
+        private static IQsiTreeNode ParseTrinoStatement(StatementContext context)
+        {
+            switch (context)
+            {
+                case InsertIntoContext insertIntoContext:
+                    return ActionVisitor.VisitInsertInto(insertIntoContext);
+
+                default:
+                    throw TreeHelper.NotSupportedTree(context);
+            }
+        }
+    }
+}
