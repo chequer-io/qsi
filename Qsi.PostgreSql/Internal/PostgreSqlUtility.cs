c1c1c997266735da1a7a0166ecdfe86314af6066
Mason Oh <challenger71498@gmail.com>
2022-03-08 18:52:49 +0900

    Modify PostgreSql parser to use antlr parser

--- Qsi.PostgreSql/Internal/PostgreSqlUtility.cs
+++ Qsi.PostgreSql/Internal/PostgreSqlUtility.cs
@@ -1,6 +1,30 @@
+using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+
 namespace Qsi.PostgreSql.Internal;
 
-public class PostgreSqlUtility
+internal static class PostgreSqlUtility
 {
-    
+    public static PostgreSqlParserInternal CreateParser(string input)
+    {
+        var stream = new AntlrInputStream(input);
+
+        var lexer = new PostgreSqlLexerInternal(stream);
+        
+        // NOTE: Add custom error listener
+
+        var tokens = new CommonTokenStream(lexer);
+
+        var parser = new PostgreSqlParserInternal(tokens)
+        {
+            Interpreter =
+            {
+                PredictionMode = PredictionMode.SLL
+            }
+        };
+        
+        // NOTE: Add custom error listener
+
+        return parser;
+    }
 }
