ad785137cd30c4fa0f7a62c278585ed2a61b833f
Tony Jang <tony@chequer.io>
2022-03-16 00:31:11 +0900

    Add ToPostgreSqlScript into RedshiftScriptParser

--- Qsi.Redshift/RedshiftScriptParser.cs
+++ Qsi.Redshift/RedshiftScriptParser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Qsi.Data;
+using Qsi.Parsing.Common;
 using Qsi.PostgreSql;
 using static Qsi.Shared.Extensions.StringExtension;
 
@@ -7,23 +8,29 @@ namespace Qsi.Redshift;
 
 public class RedshiftScriptParser : PostgreSqlScriptParser
 {
-    protected override QsiScript CreateScript(ParseContext context, IReadOnlyList<Token> tokens)
+    public QsiScript ToPostgreSqlScript(QsiScript script)
     {
-        string input = context.Cursor.Value;
-        var startIndex = tokens[0].Span.Start.GetOffset(context.Cursor.Length);
-        var endIndex = tokens[^1].Span.End.GetOffset(context.Cursor.Length) - 1;
-        var (startPosition, endPosition) = MeasurePosition(context, startIndex, endIndex);
-        var script = input[startIndex..(endIndex + 1)];
-
-        Token[] leadingTokens = GetLeadingTokens(input, tokens, TokenType.Keyword, 2);
-        var scriptType = GetSuitableType(context.Cursor, tokens, leadingTokens);
-
-        if (scriptType is QsiScriptType.Delete &&
-            (leadingTokens.Length != 2 || !input[leadingTokens[1].Span].EqualsIgnoreCase("FROM")))
+        switch (script.ScriptType)
         {
-            script = $"{input[startIndex..leadingTokens[0].Span.End]} FROM{input[leadingTokens[0].Span.End..(endIndex + 1)]}";
+            // DELETE actor;
+            // DELETE FROM actor;
+            case QsiScriptType.Delete:
+            {
+                var sql = script.Script;
+
+                IEnumerable<Token> tokens = ParseTokens(new CommonScriptCursor(script.Script));
+                Token[] leadingTokens = GetLeadingTokens(script.Script, tokens, TokenType.Keyword, 2);
+
+                if (leadingTokens.Length != 2 || !sql[leadingTokens[1].Span].EqualsIgnoreCase("FROM"))
+                    return new QsiScript(
+                        $"{sql[..leadingTokens[0].Span.End]} FROM{sql[leadingTokens[0].Span.End..]}",
+                        script.ScriptType
+                    );
+
+                break;
+            }
         }
 
-        return new QsiScript(script, scriptType, startPosition, endPosition);
+        return script;
     }
 }
