de5f33bfc6b6df35a8953fb695dd76966fc94458
Mason Oh <challenger71498@gmail.com>
2022-04-07 19:19:30 +0900

    Test: create test code for class PostgreSqlUnicodeUtility.

--- Qsi.Tests/Vendor/PostgreSQL/Internal/PostgreSqlUnicodeUtilityTest.cs
+++ Qsi.Tests/Vendor/PostgreSQL/Internal/PostgreSqlUnicodeUtilityTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -8,12 +9,42 @@ namespace Qsi.Tests.Vendor.PostgreSQL.Internal;
 [TestFixture]
 public class PostgreSqlUnicodeUtilityTest
 {
+    [Timeout(1000)]
+    [TestCase("U&\"d\\0061t\\0061\"", ExpectedResult = "\"data\"")]
+    [TestCase("U&\"d!0061 t!+000061\" UESCAPE '!'", ExpectedResult = "\"da ta\"")]
+    
+    // TODO: Seperate test cases with errors.
+    [TestCase("u&\"da0061taa a+000061 a aa aa\" UESCAPE 'a'", ExpectedResult = "")]
+    public string Test_GetString(string input)
+    {
+        var result = PostgreSqlUnicodeUtility.GetString(input);
+
+        return result;
+    }
+    
+    [Timeout(1000)]
+    [TestCase("U&\"d\\0061t\\0061\"", ExpectedResult = "d\\0061t\\0061")]
+    [TestCase("U&\"d\\0061t\\0061", ExpectedResult = "\"d\\0061t\\0061")]
+    public string Test_Dequote(string quotedInput)
+    {
+        var result = PostgreSqlUnicodeUtility.Dequote(quotedInput);
+
+        return result;
+    }
+    
+    [Timeout(1000)]
     [TestCase("d\\0061t\\0061", '\\', ExpectedResult = "data")]
     [TestCase("d!0061t!+000061", '!', ExpectedResult = "data")]
-    public string Test_ProcessUnicodeEscape(string input, char escape)
+    [TestCase("d!0061t!+000061!!", '!', ExpectedResult = "data!")]
+    [TestCase("d\\0061t\\+000061 y\\y", '\\', ExpectedResult = "data y\\y")]
+    
+    // TODO: Seperate test cases with errors.
+    [TestCase("d\\", '\\', ExpectedResult = "")]
+    [TestCase("\\\\ \\        ", '\\', ExpectedResult = "")]
+    [TestCase("da0061taa a+000061 a aa aa", 'a', ExpectedResult = "")]
+    public string Test_Parse(string input, char escape)
     {
-        var result = PostgreSqlUnicodeUtility.ProcessUnicodeEscape(input, escape, 0);
-
+        var result = PostgreSqlUnicodeUtility.Parse(input, escape, 0);
         return result;
     }
 }
