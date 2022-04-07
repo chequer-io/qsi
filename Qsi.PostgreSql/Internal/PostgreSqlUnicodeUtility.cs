465bcb5493230c3760aacaa3ea43535764abb1bd
Mason Oh <challenger71498@gmail.com>
2022-04-07 19:19:02 +0900

    Implement a fully functional logic with error handling for unicode escaped string/identifier.

--- Qsi.PostgreSql/Internal/PostgreSqlUnicodeUtility.cs
+++ Qsi.PostgreSql/Internal/PostgreSqlUnicodeUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -6,6 +7,129 @@ namespace Qsi.PostgreSql.Internal;
 
 internal static class PostgreSqlUnicodeUtility
 {
+    /// <summary>
+    /// Get a quoted string from a unicode string.
+    /// </summary>
+    /// <param name="input">Input string which contains unicodes.</param>
+    /// <param name="quote">Quote character to attach at the start and the end. Default value is &#39;&#34;&#39;.</param>
+    /// <returns>A string with quotes.</returns>
+    /// <exception cref="Exception"></exception>
+    public static string GetString(string input, char quote = '\"')
+    {
+        // If input length is invalid, return itself.
+        if (input.Length < 2)
+        {
+            return input;
+        }
+        
+        // If input is not starting with unicode symbol, return itself.
+        if (input[..2] is not "U&" and not "u&")
+        {
+            return input;
+        }
+        
+        // Split quotes and escape letter
+        var i = 2;
+        var splitted = new List<string>();
+        bool isQuoted = false;
+        var temp = "";
+        
+        while (i < input.Length)
+        {
+            if (input[i] == '"')
+            {
+                isQuoted = !isQuoted;
+            }
+
+            temp += input[i];
+            
+            if (!isQuoted)
+            {
+                break;
+            }
+
+            i++;
+        }
+
+        if (isQuoted)
+        {
+            return input;
+        }
+        
+        splitted.Add(temp);
+
+        if (i + 1 < input.Length)
+        {
+            var rest = input[(i + 1)..];
+
+            if (rest.Length != 0)
+            {
+                splitted.AddRange(rest.Split().Where(s => !string.IsNullOrWhiteSpace(s)));
+            }
+        }
+        
+        // If the number of splitted strings are invalid, return the input.
+        if (splitted.Count is not 1 and not 3)
+        {
+            Console.WriteLine($"INVALID_INPUT, {splitted.Count}");
+            return input;
+        }
+
+        // Parse splitted strings.
+        var unicode = Dequote(splitted[0]);
+        
+        string parsed;
+        
+        if (splitted.Count == 3)
+        {
+            var escape = Dequote(splitted[2]);
+
+            if (escape.Length != 1)
+            {
+                throw new Exception($"Invalid Unicode escape character \"{escape}\".");
+            }
+
+            if (escape[0] == '"')
+            {
+                throw new Exception($"Invalid Unicode escape character \"{escape}\".");
+            }
+            
+            parsed = Parse(unicode, escape[0]);
+        }
+        else
+        {
+            parsed = Parse(unicode, '\\');   
+        }
+        
+        // Add quotes to parsed string.
+        parsed = parsed.Insert(0, quote.ToString());
+        parsed += quote;
+        
+        return parsed;
+    }
+    
+    /// <summary>
+    /// Remove &#34;U&#38;&#34;, &#34;u&#38;&#34; and quotes.
+    /// </summary>
+    /// <param name="quotedInput">Input which contains escapes and quotes.</param>
+    /// <returns>A string without escapes and quotes.</returns>
+    public static string Dequote(string quotedInput)
+    {
+        if (quotedInput[..2] is "U&" or "u&")
+        {
+            quotedInput = quotedInput.Remove(0, 2);
+        }
+
+        if (quotedInput[0] is '"' or '\'' && quotedInput[^1] is '"' or '\'')
+        {
+            quotedInput = quotedInput[1..^1];
+        }
+        
+        // quotedInput = IdentifierUtility.Unescape(quotedInput);
+
+        return quotedInput;
+    }
+
     /// <summary>
     /// Process Unicode escapes, producing a usual string.
     /// </summary>
@@ -17,7 +141,7 @@ internal static class PostgreSqlUnicodeUtility
     /// <remarks>
     /// Original function is written by C++ in <a href="https://github.com/postgres/postgres/blob/f4fb45d15c59d7add2e1b81a9d477d0119a9691a/src/backend/parser/parser.c#L362">HERE</a>.
     /// </remarks>
-    public static string ProcessUnicodeEscape(string input, char escape, int position = 0)
+    public static string Parse(string input, char escape, int position = 0)
     {
         var output = "";
         int pairFirst = 0;
@@ -41,6 +165,11 @@ internal static class PostgreSqlUnicodeUtility
             }
 
             // If the next leter is also an escape, insert escape letter itself.
+            if (i + 1 >= input.Length)
+            {
+                throw new Exception("Invalid Unicode escape. Hint: Unicode escapes must be \\XXXX or \\+XXXXXX.");
+            }
+
             if (input[i + 1] == escape)
             {
                 if (pairFirst != 0)
@@ -57,7 +186,7 @@ internal static class PostgreSqlUnicodeUtility
             // If next 4 letters are numbers, insert a unicode(2 bytes).
             if (i + 5 > input.Length)
             {
-                continue;
+                throw new Exception("Invalid Unicode escape. Hint: Unicode escapes must be \\XXXX or \\+XXXXXX.");
             }
             
             var bit16 = input[(i + 1)..(i + 5)];
@@ -71,9 +200,9 @@ internal static class PostgreSqlUnicodeUtility
             }
 
             // If the next letter is plus, and also if next belonging 6 letters are numbers, insert a unicode(3 bytes).
-            if (i + 7 > input.Length)
+            if (i + 8 > input.Length)
             {
-                continue;
+                throw new Exception("Invalid Unicode escape. Hint: Unicode escapes must be \\XXXX or \\+XXXXXX.");
             }
             
             var bit24 = input[(i + 2)..(i + 8)];
@@ -83,7 +212,10 @@ internal static class PostgreSqlUnicodeUtility
                 AddUnicode(bit24, ref pairFirst, ref output);
                 
                 i += 8;
+                continue;
             }
+
+            throw new Exception("Invalid Unicode escape. Hint: Unicode escapes must be \\XXXX or \\+XXXXXX.");
         }
 
         return output;
@@ -108,12 +240,18 @@ internal static class PostgreSqlUnicodeUtility
             }
             else
             {
-                throw new Exception("INVALID_PAIR");
+                Console.WriteLine("INVALID_PAIR");
+                return;
+                
+                // throw new Exception("INVALID_PAIR");
             }
         }
         else if (is_utf16_surrogate_second(unicode))
         {
-            throw new Exception("INVALID_PAIR");
+            Console.WriteLine("INVALID_PAIR");
+            return;
+            
+            // throw new Exception("INVALID_PAIR");
         }
                 
         if (is_utf16_surrogate_first(unicode))
