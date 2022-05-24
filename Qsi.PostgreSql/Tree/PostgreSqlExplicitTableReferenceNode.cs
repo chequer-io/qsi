7f93edc467c24ea55c0a7adbe756798c985ff281
Mason Oh <challenger71498@gmail.com>
2022-05-24 16:21:26 +0900

    Add commentary; todo

--- Qsi.PostgreSql/Tree/PostgreSqlExplicitTableReferenceNode.cs
+++ Qsi.PostgreSql/Tree/PostgreSqlExplicitTableReferenceNode.cs
@@ -2,6 +2,7 @@ using Qsi.Tree;
 
 namespace Qsi.PostgreSql.Tree;
 
+// TODO: Find out what does this node stand for.
 public class PostgreSqlExplicitTableReferenceNode : QsiTableReferenceNode
 {
     
