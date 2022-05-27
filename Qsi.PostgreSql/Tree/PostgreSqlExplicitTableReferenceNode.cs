f116f43b1c8b5109ca19d277a492c7d50c1d7502
Mason Oh <challenger71498@gmail.com>
2022-05-27 15:12:34 +0900

    Remove a finished todo

--- Qsi.PostgreSql/Tree/PostgreSqlExplicitTableReferenceNode.cs
+++ Qsi.PostgreSql/Tree/PostgreSqlExplicitTableReferenceNode.cs
@@ -2,7 +2,6 @@ using Qsi.Tree;
 
 namespace Qsi.PostgreSql.Tree;
 
-// TODO: Find out what does this node stand for.
 public class PostgreSqlExplicitTableReferenceNode : QsiTableReferenceNode
 {
     
