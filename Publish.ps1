c5e7016cca03409a4bbd8eb13173fe2c59a0db18
Tony Jang <tony@chequer.io>
2022-11-10 23:11:29 +0900

    Publish.ps1 Qsi.Redshift Antlr to false

--- Publish.ps1
+++ Publish.ps1
@@ -73,7 +73,7 @@ $Tasks =
 [Task]::new("Qsi.Impala", $true),
 [Task]::new("Qsi.Trino", $true),
 [Task]::new("Qsi.Athena", $true),
-[Task]::new("Qsi.Redshift", $true)
+[Task]::new("Qsi.Redshift", $false)
 
 Function DotNet-Pack {
     Param (
