0e51e99966c97fb006030b42daf09cad5b89e425
Tony Jang <tony@chequer.io>
2023-06-29 12:14:14 +0900

    QP-4349 Fix MySQL grant RoleOrPrivilegesList NRE

--- Qsi.MySql/Tree/Visitors/ActionVisitor.cs
+++ Qsi.MySql/Tree/Visitors/ActionVisitor.cs
@@ -315,7 +315,7 @@ namespace Qsi.MySql.Tree
 
             if (context.roleOrPrivilegesList() is { } roleOrPrivilegesList)
             {
-                node.Roles.AddRange(roleOrPrivilegesList.roleOrPrivilege().Select(p => p.GetInputText()));
+                node.Roles = roleOrPrivilegesList.roleOrPrivilege().Select(p => p.GetInputText()).ToArray();
             }
 
             if (context.HasToken(ALL_SYMBOL))
