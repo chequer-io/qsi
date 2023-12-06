df2016605578c4f8d74d13d2c1ea392508b61a29
Tony Jang <tony@chequer.io>
2023-12-06 15:37:19 +0900

    Add properties to QsiUserInfo

--- Qsi/Data/Action/QsiUserInfo.cs
+++ Qsi/Data/Action/QsiUserInfo.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+
 namespace Qsi.Data;
 
 public class QsiUserInfo
 {
     public string Username { get; set; }
+
+    public Dictionary<string, object> Properties { get; } = new();
 }
