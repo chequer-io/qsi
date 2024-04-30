b5fd9e4c3122f99a2da4d41d67c3a15bf0c2f16d
Mason Oh <challenger71498@gmail.com>
2024-04-30 18:09:32 +0900

    Add indirect column at QsiTableStructure

--- Qsi/Data/Table/QsiTableStructure.cs
+++ Qsi/Data/Table/QsiTableStructure.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Qsi.Shared.Extensions;
 
@@ -17,6 +18,17 @@ public sealed class QsiTableStructure
     public IList<QsiTableStructure> References { get; } = new List<QsiTableStructure>();
 
     public IList<QsiTableColumn> Columns => _columns;
+
+    /// <summary>
+    /// 테이블을 직접적으로 구성하는 컬럼이 아닌 간접적으로 참조하는 컬럼을 의미합니다.
+    /// </summary>
+    /// <remarks>
+    /// <ul>
+    /// <li>WHERE문이나 HAVING문의 조건에서 사용하는 컬럼이나, 서브쿼리 내에 들어 있는 컬럼 등을 포함합니다.</li>
+    /// <li>주석 작성일 기준으로 해당 필드의 값은 WHERE절 내부에서 사용하는 컬럼들만 포함하고 있습니다.</li>
+    /// </ul>
+    /// </remarks>
+    public QsiTableColumn[] IndirectColumns = Array.Empty<QsiTableColumn>();
 
     internal IEnumerable<QsiTableColumn> VisibleColumns => _columns.Where(c => c.IsVisible);
 
@@ -46,6 +58,7 @@ public sealed class QsiTableStructure
 
         table.References.AddRange(References);
         table._columns.AddRange(_columns.Select(c => c.CloneInternal()));
+        table.IndirectColumns = (QsiTableColumn[])IndirectColumns.Clone();
 
         return table;
     }
@@ -62,6 +75,7 @@ public sealed class QsiTableStructure
 
         table.References.AddRange(References);
         table._columns.AddRange(VisibleColumns.Select(c => c.CloneInternal()));
+        table.IndirectColumns = (QsiTableColumn[])IndirectColumns.Clone();
 
         return table;
     }
