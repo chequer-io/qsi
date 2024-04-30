b86f3bf9644cee964e05badb146f8d20dbe7c925
Mason Oh <challenger71498@gmail.com>
2024-04-30 14:22:54 +0900

    Add debugger UI for indirect column access

---
+++ Qsi.Debugger/Models/QsiLabelTreeItem.cs
@@ -0,0 +1,11 @@
+namespace Qsi.Debugger.Models;
+
+public class QsiLabelTreeItem
+{
+    public string Label { get; }
+
+    public QsiLabelTreeItem(string label)
+    {
+        Label = label;
+    }
+}
