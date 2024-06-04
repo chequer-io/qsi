9ac2bdc51a2f12ee645f66921f7feaaf03c5409e
Mason Oh <challenger71498@gmail.com>
2024-06-04 14:56:49 +0900

    Remove redundant MySQL options

--- Qsi.SingleStore/Diagnostics/MySqlRawParser.cs
+++ Qsi.SingleStore/Diagnostics/MySqlRawParser.cs
@@ -7,18 +7,9 @@ namespace Qsi.SingleStore.Diagnostics;
 
 public class SingleStoreRawParser : AntlrRawParserBase
 {
-    private readonly int _version;
-    private readonly bool _mariaDbCompatibility;
-
-    public SingleStoreRawParser(Version version, bool mariaDBCompatibility)
-    {
-        _version = SingleStoreUtility.VersionToInt(version);
-        _mariaDbCompatibility = mariaDBCompatibility;
-    }
-
     protected override (ITree Tree, string[] RuleNames) ParseAntlrTree(string input)
     {
-        var parser = SingleStoreUtility.CreateParser(input, _version, _mariaDbCompatibility);
+        var parser = SingleStoreUtility.CreateParser(input);
         return (parser.query(), parser.RuleNames);
     }
 }
