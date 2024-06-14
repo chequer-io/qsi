f1603ed34f4d81785efde5ee199cbc9128875a3d
Mason Oh <challenger71498@gmail.com>
2024-06-14 14:15:36 +0900

    Remove sensitive data logic from MySQL analyzer
    민감 데이터 마스킹은 MySQL에서만 지원하므로 SingleStore에서는 제거합니다.

--- Qsi.SingleStore/Analyzers/SingleStoreActionAnalyzer.cs
+++ Qsi.SingleStore/Analyzers/SingleStoreActionAnalyzer.cs
@@ -20,36 +20,6 @@ public class SingleStoreActionAnalyzer : QsiActionAnalyzer
     {
     }
 
-    protected override async ValueTask<IQsiAnalysisResult[]> ExecuteDataUpdateAction(IAnalyzerContext context, IQsiDataUpdateActionNode action)
-    {
-        IQsiAnalysisResult[] result = await base.ExecuteDataUpdateAction(context, action);
-
-        if (result.FirstOrDefault() is { } firstResult &&
-            action.Target is IQsiDerivedTableNode { Where: { } whereExpr })
-        {
-            IEnumerable<IQsiExpressionNode> nodes = action.SetValues
-                .Select(values => values.Value)
-                .Append(whereExpr.Expression);
-
-            AddSensitiveData(firstResult.SensitiveDataCollection, nodes);
-        }
-
-        return result;
-    }
-
-    protected override async ValueTask<IQsiAnalysisResult[]> ExecuteDataDeleteAction(IAnalyzerContext context, IQsiDataDeleteActionNode action)
-    {
-        IQsiAnalysisResult[] result = await base.ExecuteDataDeleteAction(context, action);
-
-        if (result.FirstOrDefault() is { } firstResult &&
-            action.Target is IQsiDerivedTableNode { Where: { } whereExpr })
-        {
-            AddSensitiveData(firstResult.SensitiveDataCollection, whereExpr.Expression);
-        }
-
-        return result;
-    }
-
     protected override QsiDataValue ResolveColumnValue(IAnalyzerContext context, IQsiExpressionNode expression)
     {
         if (expression is IQsiLiteralExpressionNode { Value: SingleStoreString singleStoreString })
@@ -58,53 +28,5 @@ public class SingleStoreActionAnalyzer : QsiActionAnalyzer
         }
 
         return base.ResolveColumnValue(context, expression);
-    }
-
-    protected override QsiVariableSetActionResult ResolveVariableSet(IAnalyzerContext context, IQsiVariableSetItemNode node)
-    {
-        var result = base.ResolveVariableSet(context, node);
-
-        if (result.Name.Value is "PASSWORD")
-            result.SensitiveDataCollection.Add(CreateSensitiveData(QsiSensitiveDataType.Password, node.Expression));
-
-        return result;
-    }
-
-    protected override QsiUserInfo ResolveUser(IAnalyzerContext context, IQsiUserNode node, QsiSensitiveDataCollection dataCollection)
-    {
-        var user = base.ResolveUser(context, node, dataCollection);
-
-        if (node.Password is not null)
-        {
-            dataCollection.Add(CreateSensitiveData(QsiSensitiveDataType.Password, node.Password));
-            user.Properties[User.IsRandomPassword] = false;
-        }
-        else
-        {
-            user.Properties[User.IsRandomPassword] = node is SingleStoreUserNode { IsRandomPassword: true };
-        }
-
-        return user;
-    }
-
-    private void AddSensitiveData(QsiSensitiveDataCollection collection, IEnumerable<IQsiExpressionNode> nodes)
-    {
-        AddSensitiveData(collection, nodes.ToArray());
-    }
-
-    private void AddSensitiveData(QsiSensitiveDataCollection collection, params IQsiExpressionNode[] nodes)
-    {
-        IEnumerable<QsiSensitiveData> sensitiveDatas = nodes
-            .SelectMany(n => n.Flatten())
-            .Select(e => (e, SingleStoreTree.SensitiveType[e]))
-            .Where(e => e.Item2 is not QsiSensitiveDataType.None)
-            .Select(e => CreateSensitiveData(e.Item2, e.e));
-
-        collection.AddRange(sensitiveDatas);
-    }
-
-    protected override QsiSensitiveData CreateSensitiveData(QsiSensitiveDataType dataType, IQsiTreeNode node)
-    {
-        return new QsiSensitiveData(dataType, SingleStoreTree.Span[node]);
     }
 }
